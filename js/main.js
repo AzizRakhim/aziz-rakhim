a2289d6a348fb2536c61779044a542c880e31154
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-02-11 23:56:04 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let elSettingBtn = document.getElementById("settingBtn");
+let elSettingBox = document.getElementById("settingBox");
+
+elSettingBtn.addEventListener("click", function() {
+  elSettingBox.classList.toggle("show");
+})
+
+
